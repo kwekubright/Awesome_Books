75ad44ad4ba38f0792167ed1549a89228b8994e9
Graycemuthui <graycemuthui20@gmail.com>
2022-05-23 21:30:03 +0300

    Add package.json and fix linters

--- js/books.js
+++ js/books.js
@@ -5,12 +5,30 @@ function BookSkeleton(title, author) {
   this.added_at = new Date().toLocaleDateString();
 }
 
+let books = [];
+/* eslint-disable no-restricted-syntax */
+
+function displayBooks() {
+  let bookHtml = "";
+  for (let i = 0; i < books.length; i += 1) {
+    bookHtml += ` 
+    <div id="book-${i}">   
+      <div class="book">
+        <h3 class="title">${books[i].title} </h3>
+        <p class="author">${books[i].author} </p>
+        <button onclick="removeBook(${i})">Remove</button>
+      </div>
+      <hr>
+    </div>`;
+  }
+  document.querySelector("#books-container").innerHTML = bookHtml;
+}
+
 // Lets define our collection of books
 const hasBooks = localStorage.getItem("books");
 
 // Lets create initial collection if there are no books in storage
 if (!hasBooks) {
-  let books = [];
   books.push(
     new BookSkeleton("Book 1", "Author 1"),
     new BookSkeleton("Book 2", "Author 2"),
@@ -33,6 +51,8 @@ function addBook(title, author) {
   displayBooks();
 }
 
+/* eslint-disable */
+
 // Lets define our remove book function
 function removeBook(key) {
   // lets remove the book
@@ -42,47 +62,33 @@ function removeBook(key) {
   // lets update the storage
   localStorage.setItem("books", JSON.stringify(books));
   // lets update the page
-  const id = "book-" + key;
+  const id = `book-${key}`;
   document.getElementById(id).remove();
   displayBooks();
 }
 
-function displayBooks() {
-  let bookHtml = "";
-  for (let i = 0; i < books.length; i++) {
-    bookHtml += ` 
-    <div id="book-${i}">   
-      <div class="book">
-        <h3 class="title">${books[i].title} </h3>
-        <p class="author">${books[i].author} </p>
-        <button onclick="removeBook(${i})">Remove</button>
-      </div>
-      <hr>
-    </div>`;
-  }
-  document.querySelector("#books-container").innerHTML = bookHtml;
-}
+/* eslint-enable */
 
 // Lets get the add book button
 const addButton = document.querySelector("#add-button");
 // Lets add a click event listener with which we will perform the add logic for the new book
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
-  form = document.getElementById("add-book-form");
+  const form = document.getElementById("add-book-form");
   if (!form.reportValidity()) {
     return;
   }
   // lets get the new title
-  newTitle = document.querySelector("#book-title-input").value;
+  const newTitle = document.querySelector("#book-title-input").value;
   // now lets get the new author
-  newAuthor = document.querySelector("#book-author-input").value;
+  const newAuthor = document.querySelector("#book-author-input").value;
   // lets add it to the books object at this point
   addBook(newTitle, newAuthor);
   // okay great. Lets reset the form now
   form.reset();
-  console.log(1);
-  console.log(newTitle);
-  console.log(newAuthor);
 });
 
-displayBooks();
+// load the form fields from local storage
+window.onload = () => {
+  displayBooks();
+};
